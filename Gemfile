e6c5d2cf8b32dafa1428cf30e32135e2bf32735d
Ngoc Tuan Truong <truongtuan97@gmail.com>
2019-07-29 21:13:04 +0700

    implement creation process for posts and refactored

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.0'
   gem 'capybara'
   gem 'database_cleaner'
-  gem 'factory_girl_rails', '~> 4.7'
+  gem 'factory_bot'
 end
 
 group :development do
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
 describe "navigate" do
-  describe 'homepage' do
-    it 'can be reached successfully' do
+  describe 'index' do
+    before do
       visit posts_path
+    end
+    it 'can be reached successfully' do      
       expect(page.status_code).to eq(200)
     end
 
-    it 'has a title of Posts' do
-      visit posts_path
+    it 'has a title of Posts' do      
       expect(page).to have_content(/Posts/)
+    end
+  end
+
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+    it 'has a new form that can be reached' do      
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do      
+      fill_in "post[date]",	with: Date.today
+      fill_in "post[rationale]", with: 'Some rationale'
+
+      click_on 'Save'
+      expect(page).to have_content('Some rationale')
     end
   end
 end
